c30f34a3a8e7115a442851818b675d5bf1b2ddcb
Naveen <naveensrinivas145@gmail.com>
2025-12-20 14:34:32 +0530

    msg

--- front/pro/src/pages/Login/Login.jsx
+++ front/pro/src/pages/Login/Login.jsx
@@ -1,8 +1,12 @@
+import React from 'react';
+import './Login.css';
 const Login = () => {
   return (
     <>
     
       <div className="home-content">
+
+        <div className="login-box">
       <label>Name:</label>
       <input type="text" name="name" />
       <br />
@@ -10,6 +14,7 @@ const Login = () => {
       <input type="password" name="password" />
       <br />
       <button type="submit">Login</button>  
+      </div>
       </div>
     </>
   );
