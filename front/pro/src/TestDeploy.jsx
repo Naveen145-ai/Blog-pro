819025a72a16e0a80f49a8cdeb6807ded72317b8
EC2 Default User <ec2-user@ip-172-31-67-130.ec2.internal>
2026-02-10 05:28:38 +0000

    msg

---
+++ front/pro/src/TestDeploy.jsx
@@ -0,0 +1,10 @@
+function TestDeploy() {
+  return (
+    <div style={{ padding: "20px", backgroundColor: "#e6fffa" }}>
+      <h2>✅ Deployment Test Successful</h2>
+      <p>This file was created on EC2 and pushed to GitHub.</p>
+    </div>
+  );
+}
+
+export default TestDeploy;
